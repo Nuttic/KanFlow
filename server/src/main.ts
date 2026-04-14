cd38705315e66f3515940e79b6910e88e1d10098
Netta <netta@MacBook-Air-de-Netta.local>
2026-04-14 13:03:16 +0300

    add: swagger and changed dto

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser'; // Не забудь установить и импортировать!
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('KanFlow API')
+    .setDescription('The KanFlow project API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Если используешь JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Это создаст маршрут http://localhost:3000/api
+  SwaggerModule.setup('api', app, document);
 
   // Настройка CORS для работы с фронтендом
   app.enableCors({
@@ -18,6 +32,8 @@ async function bootstrap() {
     // Методы, которые разрешены
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   // ВАЖНО: Для работы с куками в NestJS нужно подключить cookie-parser
   // Установи его: npm install cookie-parser
--- server/src/modules/auth/dto/registr.dto.ts
+++ server/src/modules/auth/dto/registr.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ required: false })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'user@example.com' })
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
+  @ApiProperty({ example: 'password123' })
   password: string;
 }
