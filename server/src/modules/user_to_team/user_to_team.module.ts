5845983b394f7cd6abe8ce66db4fb99496df95de
Netta <netta@MacBook-Air-de-Netta.local>
2026-04-29 10:21:32 +0300

    Added column color in collection entity

--- server/src/modules/user_to_team/user_to_team.module.ts
+++ server/src/modules/user_to_team/user_to_team.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AccessService } from './user_to_team.service';
 import { AccessController } from './user_to_team.controller';
-import { User_to_team_access } from 'src/db/entities/user_to_team_access.entity';
-import { Team } from 'src/db/entities/team.entity';
+import { User_to_team_access } from '../../db/entities/user_to_team_access.entity';
+import { Team } from '../../db/entities/team.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
